ca9364df5b868fbec6a37babaa9cb305e65f63a8
AlperZM <35838758+AlperZM@users.noreply.github.com>
2023-09-22 11:56:01 +0300

    Update footer-about-title-logo.php

--- Seges One Fse/inc/patterns/footer-about-title-logo.php
+++ Seges One Fse/inc/patterns/footer-about-title-logo.php
@@ -9,12 +9,12 @@ return array(
 	'content'    => '<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var(--wp--custom--spacing--large, 8rem)","bottom":"6rem"}}},"backgroundColor":"secondary","layout":{"inherit":true}} -->
 					<div class="wp-block-group alignfull has-secondary-background-color has-background" style="padding-top:var(--wp--custom--spacing--large, 8rem);padding-bottom:6rem"><!-- wp:columns {"align":"wide"} -->
 					<div class="wp-block-columns alignwide"><!-- wp:column {"width":"33%"} -->
-					<div class="wp-block-column" style="flex-basis:33%"><!-- wp:paragraph {"style":{"typography":{"textTransform":"uppercase"}}} -->
-					<p style="text-transform:uppercase">' . esc_html__( 'About us', 'seges_one_fse' ) . '</p>
+					<div class="wp-block-column" style="flex-basis:33%"><!-- wp:paragraph -->
+					<p>' . esc_html__( 'About us', 'seges_one_fse' ) . '</p>
 					<!-- /wp:paragraph -->
 
 					<!-- wp:paragraph {"style":{"fontSize":"small"} -->
-					<p class="has-small-font-size">' . esc_html__( 'We are a rogue collective of bird watchers. We’ve been known to sneak through fences, climb perimeter walls, and generally trespass in order to observe the rarest of birds.', 'seges_one_fse' ) . '</p>
+					<p class="has-small-font-size">' . esc_html__( 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.', 'seges_one_fse' ) . '</p>
 					<!-- /wp:paragraph -->
 
 					<!-- wp:spacer {"height":180} -->
